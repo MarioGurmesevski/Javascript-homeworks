0d5d4086d657edda4af61afb5e86391012c466aa
someoneStoleEbruio <mariogurmesevski1@gmail.com>
2022-12-04 14:03:53 +0100

    Homework 1

---
+++ 1st class hw/app.js
@@ -0,0 +1,7 @@
+console.log("Homework")
+let totalPhones = 30;
+let priceOfOnePhone = 119.95;
+let taxRate = 5;
+let totalPriceOfAllPhones = (30*119.95);
+let totalTaxOfPhones = ((taxRate/100)*totalPriceOfAllPhones)
+console.log (totalPriceOfAllPhones+totalTaxOfPhones)
