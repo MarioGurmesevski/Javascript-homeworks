19edc64fde1f1d8a894abb990d4d93ec1b88e1cc
Mario Gurmesevski <119735354+MarioGurmesevski@users.noreply.github.com>
2022-12-13 18:00:39 +0100

    Update app.js

--- 1st class hw/app.js
+++ 1st class hw/app.js
@@ -2,6 +2,6 @@ console.log("Homework")
 let totalPhones = 30;
 let priceOfOnePhone = 119.95;
 let taxRate = 5;
-let totalPriceOfAllPhones = (30*119.95);
+let totalPriceOfAllPhones = (totalPhones*priceOfOnePhone);
 let totalTaxOfPhones = ((taxRate/100)*totalPriceOfAllPhones)
 console.log (totalPriceOfAllPhones+totalTaxOfPhones)
